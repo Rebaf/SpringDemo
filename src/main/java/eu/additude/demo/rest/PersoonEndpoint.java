291db962bac384f14f5dd09a03bbae47236db0c6
Rebaf <cor.faber@additude.eu>
2020-04-23 22:40:22 +0200

    Implementatie van DTO om niet alle persoonsgegevens vrij te geven.

--- src/main/java/eu/additude/demo/rest/PersoonEndpoint.java
+++ src/main/java/eu/additude/demo/rest/PersoonEndpoint.java
@@ -1,6 +1,7 @@
 package eu.additude.demo.rest;
 
 import eu.additude.demo.controller.PersoonService;
+import eu.additude.demo.dto.PersoonDTO;
 import eu.additude.demo.model.Persoon;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,12 +17,12 @@ public class PersoonEndpoint {
     PersoonService service; //= new PersoonService();
 
     @GetMapping("getPersoon/{id}")  // org.springframework
-    public @ResponseBody ResponseEntity<Persoon> getPersoonById(@PathVariable Long id) {
+    public @ResponseBody ResponseEntity<PersoonDTO> getPersoonById(@PathVariable Long id) {
         System.out.println("LOG- REST: getPersoonById/" + id + " - Aanroep van onze restserivce voor het opvragen van één persoon.");
         Persoon persoon = service.findPersoonById(id);
 
         if (persoon != null) {
-            return new ResponseEntity<>(persoon, HttpStatus.OK);
+            return new ResponseEntity<>(new PersoonDTO(persoon), HttpStatus.OK);
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
