829f6666fa2c15bff66caa5986ad7d91b0621bcf
Johandenboer <johan.den.boer@ict.nl>
2020-04-29 14:31:34 +0200

    -

--- src/main/java/eu/additude/demo/model/validations/Age.java
+++ src/main/java/eu/additude/demo/model/validations/Age.java
@@ -1,4 +1,26 @@
 package eu.additude.demo.model.validations;
 
-public class Age {
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = AgeValidator.class)
+@Target({ElementType.FIELD, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Age {
+
+    // Is verplicht maar niet gebruiken
+    Class<?>[] groups() default {};
+
+    // Is verplicht maar niet gebruiken
+    Class<? extends Payload>[] payload() default {};
+
+    String message();
+
+    int min();
+
+    int max();
 }
